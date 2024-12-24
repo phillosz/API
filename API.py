56997f14091f7a393c5fe97221779eb072b6c918
Filip Tichý <filiptichy888@gmail.com>
2024-12-24 13:38:41 +0100

    .

--- API.py
+++ API.py
@@ -1,3 +1,12 @@
-# import requests
+import requests
 # response = requests.get("https://randomuser.me/api/")
 # print(response.text)
+
+response = requests.get("https://api.thecatapi.com/v1/breedz")
+response
+
+print(response.status_code)
+
+print(response.reason)
+
+print(response.request.headers)
