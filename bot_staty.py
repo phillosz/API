d15326663125e20880b14c7209e985a47fc2c2da
Filip Tichý <filiptichy888@gmail.com>
2025-01-16 11:25:16 +0100

    zpatky na verzi bez casu

--- bot_staty.py
+++ bot_staty.py
@@ -2,16 +2,11 @@ import discord
 from discord.ext import commands
 import requests
 import time
-import schedule
-import os
-import sys
 from datetime import datetime
-
 # Nastavení bota
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
-
 # Funkce pro získání dat z API
 def get_data(url):
     response = requests.get(url)
@@ -19,7 +14,6 @@ def get_data(url):
         return response.json()
     else:
         return None
-
 async def fetch_player_data(player_name, date_from, date_to):
     timestamp = int(time.time() * 1000)
     
@@ -27,7 +21,6 @@ async def fetch_player_data(player_name, date_from, date_to):
     url_response = get_data(base_url)
     if not url_response:
         return "Chyba při načítání dat."
-
     data = url_response["data"]
     player_data = {}
     for player in data:
@@ -52,13 +45,11 @@ async def fetch_player_data(player_name, date_from, date_to):
         "maximum_per_leg": f"https://app.dartsorakel.com/api/stats/player?rankKey=1055&showStatsBreakdown=0&playerKeyToHighlight={player_key}&minMatches=200&limit=32&_={timestamp}",
         "maximum_per_leg_actual": f"https://app.dartsorakel.com/api/stats/player?dateFrom={date_from}&dateTo={date_to}&rankKey=1055&organStat=All&tourns=All&minMatches=200&tourCardYear=&showStatsBreakdown=0&_={timestamp}"
         }
-
     for stat_name, url in stats_urls.items():
         stat_data = get_data(url)["data"]
         for player in stat_data:
             if player['player_name'] == player_name:
                 player_data[player_name][stat_name] = player["stat"]
-
     # Výstup
     player = player_data[player_name]
     result = (
@@ -70,7 +61,6 @@ async def fetch_player_data(player_name, date_from, date_to):
         f"Maximums per Leg: {player.get('maximum_per_leg', 'N/A')} (Actual: {player.get('maximum_per_leg_actual', 'N/A')})\n"
     )
     return result
-
 # Příkaz pro získání statistik
 @bot.command(name="stats")
 async def stats_command(ctx, player_name: str, date_from: str, date_to: str):
@@ -81,34 +71,12 @@ async def stats_command(ctx, player_name: str, date_from: str, date_to: str):
     except ValueError as e:
         await ctx.send(f"Chyba ve formátu dat: {e}\nZadejte data ve formátu YYYY-MM-DD.")
         return
-
     # Opravený způsob předávání celého jména
     result = await fetch_player_data(player_name, date_from, date_to)
     await ctx.send(result)
-
-
 # Testovací příkaz
 @bot.command(name="ping")
 async def ping(ctx):
     await ctx.send("Pong!")
-    
-# Funkce pro vypnutí bota
-def stop_bot():
-    print("Bot se vypíná...")
-    sys.exit("Bot se vypnul.")  # Vypne celý proces a šetří čas na Railway
-
-# Funkce pro spuštění bota
-def start_bot():
-    print("Bot se spouští...")
-    bot.run("MTMyNjkxMDY4MjA0NTc0MzE1NA.G4W2-Y.H4jux_lnuRTpkxDJrMXUMgNcQ7nqFkY7qPGZcs")  # Nahraď token správnou proměnnou nebo proměnnou prostředí
-
-# Naplánuj vypnutí a zapnutí bota
-schedule.every().day.at("17:10").do(stop_bot)  # Vypne bota v 1 ráno
-schedule.every().day.at("17:15").do(start_bot)  # Spustí bota v 6 ráno
-
-# Spusť bota poprvé
-if __name__ == "__main__":
-    start_bot()  # Spustí bota při startu
-    while True:  # Kontroluje naplánované úkoly
-        schedule.run_pending()
-        time.sleep(1)
+# Spuštění bota
+bot.run("MTMyNjkxMDY4MjA0NTc0MzE1NA.G4W2-Y.H4jux_lnuRTpkxDJrMXUMgNcQ7nqFkY7qPGZcs")
