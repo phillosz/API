00489213e394080084dd1d6e49efb9144899957f
Filip Tichý <filiptichy888@gmail.com>
2025-01-19 23:04:58 +0100

    editing fetch last matches

--- bot_staty.py
+++ bot_staty.py
@@ -45,35 +45,16 @@ async def fetch_additional_stats(player_key):
 
     return additional_stats
 
-async def fetch_last_matches(player_name, date_from, date_to):
+async def fetch_last_matches(player_key, date_from, date_to):
     timestamp = int(datetime.now().timestamp() * 1000)
     
-    players_url = "https://app.dartsorakel.com/dropdownDataSearch"
-    url_response = await get_data(players_url)
-    if not url_response:
-        return None
-    
-    data = url_response.copy()
-    player_data = {}
-    
-    for player in data:
-        player_data[player['player_name']] = {
-            'player_name': player['player_name'],
-            'player_key': player['player_key']
-    }
-    
-    #if player_name not in player_data:
-       # return None
-
-    player_key = player_data[player_name]["player_key"]
-    
     url = f"https://app.dartsorakel.com/api/player/matches/{player_key}?dateFrom={date_from}&dateTo={date_to}&rankKey=26&organStat=All&tourns=All&_={timestamp}"
-    data = await get_data(url)
-    #if not data:
-        #return None
+    url_response = await get_data(url)
 
+    data = url_response.copy()
     last_matches = []
-    for match in data.get("matches", []):  # Adjust to new data structure
+    
+    for match in data:
         legs = match["loser_score"] + match["winner_score"]
         last_matches.append({
             "date": match["match_date"],
