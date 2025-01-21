1fef995468bb13f448638da8c3f4aa56a7484129
Filip Tichý <filiptichy888@gmail.com>
2025-01-21 21:55:30 +0100

    refactor: remove .env from .gitignore and update bot initialization

--- bot_staty.py
+++ bot_staty.py
@@ -1,12 +1,16 @@
 import os
-from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 from datetime import datetime, timedelta
 import aiohttp
 from bs4 import BeautifulSoup
 
-load_dotenv()
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
